4391375017768ea724994df98d763d1befb89ad2
Matheus <joaosobramatheus@gmail.com>
2025-11-21 00:04:16 -0300

    <feat> Adicionando o construtor da classe pedido, junto ao map, classe mais complexa do código

--- vendas/src/main/java/br/icev/vendas/Pedido.java
+++ vendas/src/main/java/br/icev/vendas/Pedido.java
@@ -1,14 +1,23 @@
 package main.java.br.icev.vendas;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Pedido {
     public enum Status { PAGO }
 
+    private final Map<String, Integer> itensPorCodigo;
+    private final BigDecimal totalPago;
+    private final String codigoAutorizacao;
+    private final Status status;
+
     public Pedido(Map<String, Integer> itensPorCodigo, BigDecimal totalPago,
                   String codigoAutorizacao, Status status) {
-        throw new UnsupportedOperationException("TODO");
+        this.itensPorCodigo = new HashMap<>(itensPorCodigo);
+        this.totalPago = totalPago;
+        this.codigoAutorizacao = codigoAutorizacao;
+        this.status = status;
     }
 
     public BigDecimal getTotalPago() { throw new UnsupportedOperationException("TODO"); }
